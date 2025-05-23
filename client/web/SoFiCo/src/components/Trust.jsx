41cf54895dd4048f017158cc12f573d3eec11258
Abhishek Raj <abhishekraj995555@gmail.com>
2025-05-23 15:10:26 +0530

    trust page updated

--- client/web/SoFiCo/src/components/Trust.jsx
+++ client/web/SoFiCo/src/components/Trust.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
+import trustImg from "../assets/trust.svg"
 
 function Trust() {
   return (
-    <div className='min-h-[80vh] bg-[#10293E]'>
-        <div className="left_side text-white">
-            <p>
+    <div className='h-[80vh] bg-[#10293E] flex p-10'>
+        <div className="left_side w-1/2">
+            <img src={trustImg} className='w-full h-full' />
+        </div>
+        <div className="right_side w-1/2 text-white">
+          <p className='bg-[#1B3D5E] py-1 px-2 rounded-full font-semibold text-sm text-green-300 inline-block my-2'>
                 ⚠️TRUST & SECURITY
             </p>
-            <h1>WE VALUE YOUR TRUST<br/>AND SECURITY</h1>
-            <p>Our Mission Is To Make Finance More Accessible, Transparent, And Secure For Everyone. With Cutting.</p>
-            <button className='text-white cursor-pointer hover:bg-[#61CA53] bg-green-400 py-2 px-4 rounded-full font-bold text-lg'>GET STARTED</button>
+            <h1 className='font-extrabold text-5xl mt-4 mb-2 leading-15'>WE VALUE YOUR TRUST<br/>AND SECURITY</h1>
+            <p className='text-lg mt-2 mb-5 font-semibold'>Our Mission Is To Make Finance More Accessible, Transparent, And Secure For Everyone. With Cutting.</p>
+            <button className='text-white cursor-pointer hover:bg-[#61CA53] bg-green-400 py-2 px-4 rounded-full font-bold text-base'>GET STARTED</button>
         </div>
-        <div className="right_side"></div>
     </div>
   )
 }
